ca68bf648aef9728c68a0eddff5aea13d0c1432b
nephts <nephts@mailbox.org>
2024-06-29 14:54:47 +0100

    add dev and test tools to pyproject.toml, format with ruff

--- klearn_tcyclone/utils.py
+++ klearn_tcyclone/utils.py
@@ -1,11 +1,23 @@
+"""Utils for klearn_tcyclone."""
+
 import numpy as np
 from climada.hazard import TCTracks
 
+
 def check_time_steps_TCTracks(tc_tracks: TCTracks, time_step_h: int) -> bool:
+    """Check time steps in TCTracks.
+
+    Args:
+        tc_tracks (TCTracks): _description_
+        time_step_h (int): _description_
+
+    Returns:
+        bool: _description_
+    """
     tc_track_data = tc_tracks.data
     is_close = []
     for tc_track in tc_track_data:
         times = tc_track.time.data
-        time_diffs_h = np.array(np.diff(times), dtype=np.float64)/1E9/3600
-        is_close.append(np.allclose(time_diffs_h, time_step_h, atol=1E-21))
+        time_diffs_h = np.array(np.diff(times), dtype=np.float64) / 1e9 / 3600
+        is_close.append(np.allclose(time_diffs_h, time_step_h, atol=1e-21))
     return np.all(is_close)
--- tests/test_utils.py
+++ tests/test_utils.py
@@ -1,4 +1,3 @@
-
 from klearn_tcyclone.utils import check_time_steps_TCTracks
 from climada.hazard import TCTracks
 import xarray as xr
@@ -7,34 +6,40 @@ import numpy as np
 
 
 def test_check_time_steps_TCTracks():
-    tc_tracks = TCTracks.from_ibtracs_netcdf(provider='usa', year_range=(1993, 1994), basin='EP', correct_pres=False)
+    """Test."""
+    tc_tracks = TCTracks.from_ibtracs_netcdf(
+        provider="usa", year_range=(1993, 1994), basin="EP", correct_pres=False
+    )
     check = check_time_steps_TCTracks(tc_tracks, time_step_h=3)
-    assert check == False
+    assert not check
     tc_tracks.equal_timestep(3)
     check = check_time_steps_TCTracks(tc_tracks, time_step_h=3)
-    assert check == True
+    assert check
 
 
 def test_check_time_steps_TCTracks_2():
-
-    hours = range(0,33, 3)
-    times = np.array([dt.datetime(2000, 1, 1) + dt.timedelta(hours=x) for x in hours], dtype=np.datetime64)
-    lat = np.random.normal(0,1,len(times))
-    lon = np.random.normal(0,1,len(times))
-    data = np.random.normal(0,1,len(times))
+    """Test."""
+    hours = range(0, 33, 3)
+    times = np.array(
+        [dt.datetime(2000, 1, 1) + dt.timedelta(hours=x) for x in hours],
+        dtype=np.datetime64,
+    )
+    lat = np.random.normal(0, 1, len(times))
+    lon = np.random.normal(0, 1, len(times))
+    data = np.random.normal(0, 1, len(times))
 
     test_data = xr.DataArray(
         dims=["time"],
-        coords=dict(
-            time=times,
-            lat = ("time", lat),
-            lon = ("time", lon),
-        ),
+        coords={
+            "time" : times,
+            "lat" : ("time", lat),
+            "lon" : ("time", lon),
+        },
         data=data,
     )
 
     test_tc_track = TCTracks(data=[test_data, test_data])
     check = check_time_steps_TCTracks(test_tc_track, time_step_h=3)
-    assert check == True
+    assert check
     check = check_time_steps_TCTracks(test_tc_track, time_step_h=2)
-    assert check == False
+    assert not check
